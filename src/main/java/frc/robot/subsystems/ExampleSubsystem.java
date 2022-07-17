28d12da1c112865879ecc62349fa758427e481c3
Dylan Watson <dylantrwatson@gmail.com>
2022-07-16 22:12:50 -0500

    Installed CTRE library and created example subsystem

--- src/main/java/frc/robot/subsystems/ExampleSubsystem.java
+++ src/main/java/frc/robot/subsystems/ExampleSubsystem.java
@@ -1,15 +1,34 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
+// Adapted by Dylan Watson
 
 package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ExampleSubsystem extends SubsystemBase {
+  
+  public VictorSPX motor; // This is the motor controller
+  
   /** Creates a new ExampleSubsystem. */
-  public ExampleSubsystem() {}
+  public ExampleSubsystem() {
+    motor = new VictorSPX(0); // Create the new motor controller (make sure you check your ID!)
+  }
 
+  // This could be "runintake" or "stopintake" or "liftclimber"
+  public void runMotor(double power) {
+    motor.set(ControlMode.PercentOutput, power); // This is how you set power to a VictorSPX or a TalonSRX
+  }
+
+  public void stopMotor() {
+    runMotor(0); // motor.set(ControlMode.PercentOutput, 0)
+  }
+
+  // DON'T WORRY ABOUT periodic() and simulationPeriodic() right now
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
