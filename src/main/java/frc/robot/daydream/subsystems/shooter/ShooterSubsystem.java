e215f8f4d093c154330b86253bd11537d7d83c77
smartyvv <107887886+smartyvv@users.noreply.github.com>
2022-09-17 15:34:22 -0500

    Started PID on shooter

--- src/main/java/frc/robot/daydream/subsystems/shooter/ShooterSubsystem.java
+++ src/main/java/frc/robot/daydream/subsystems/shooter/ShooterSubsystem.java
@@ -12,6 +12,7 @@
 package frc.robot.daydream.subsystems.shooter;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.StatusFrame;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -26,20 +27,37 @@ public class ShooterSubsystem extends SubsystemBase {
         shooterMotorRight = new TalonSRX(1);
 
         shooterMotorLeft.setInverted(true);
+        shooterMotorLeft.follow(shooterMotorRight);
+        
+        shooterMotorRight.selectProfileSlot(2,0); // First parameter "2" correlates to velocity, second parameter correlates to primary PID
+        shooterMotorRight.config_kP(0, 0);  // First parameter is primary PID, second parameter is velocity
+        shooterMotorRight.config_kI(0, 0);  
+        shooterMotorRight.config_kD(0, 0);
+        shooterMotorRight.config_kF(0, 0);
+
+        shooterMotorRight.config_IntegralZone(0, 300);
+        shooterMotorRight.configAllowableClosedloopError(0, 0);
+        shooterMotorRight.configClosedLoopPeriod(0, 1);
+
+        shooterMotorRight.setStatusFramePeriod(StatusFrame.Status_2_Feedback0, 20, 50);
+        shooterMotorRight.setStatusFramePeriod(StatusFrame.Status_13_Base_PIDF0, 20, 50);
     }
 
     // This could be "runintake" or "stopintake" or "liftclimber"
+    // https://docs.ctre-phoenix.com/en/stable/ch16_ClosedLoop.html#closed-loop-configs-per-slot-four-slots-available
 
-    public void shoot(double power) {
-        ShooterPolicy.powerShooter = power;
+    public void shoot() {
         shooterMotorRight.set(ControlMode.PercentOutput, ShooterPolicy.getShooterPower());
-        shooterMotorLeft.set(ControlMode.PercentOutput, ShooterPolicy.getShooterPower());
-
 
     }
 
+    public void shootPID(){
+        shooterMotorRight.set(ControlMode.Velocity, ShooterPolicy.targetSpeed);
+    }
+
     public void stopShooter() {
-        shoot(0);
+        ShooterPolicy.powerShooter = 0;
+        shoot();
     }
 
     @Override
