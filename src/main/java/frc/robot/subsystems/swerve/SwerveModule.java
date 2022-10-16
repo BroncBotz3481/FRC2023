53fa0d15bb2f18a74a1414c080b0db80fffa5721
Mihir Ghawghawe <mihirg.301@gmail.com>
2022-10-15 23:33:00 -0500

    Completed writing code for SwerveModule.java (Needs to be checked) Added new file SwerveSubsystem.java Deleted HolonomicDriveCommand.java

--- src/main/java/frc/robot/subsystems/swerve/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/swerve/SwerveModule.java
@@ -5,6 +5,10 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import com.revrobotics.RelativeEncoder;
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Constants.ModuleConstants;
 
 public class SwerveModule {
@@ -22,8 +26,8 @@ public class SwerveModule {
     private final double absoluteEncoderOffsetRad;
 
     public SwerveModule(int driveMotorId, int turningMotorId, boolean driveMotorReversed, boolean turningMotorReversed,
-            int absoluteEncoderId,double absoluteEncoderOffset, boolean absoluteEncoderReversed) {
-        
+                        int absoluteEncoderId, double absoluteEncoderOffset, boolean absoluteEncoderReversed) {
+
         this.absoluteEncoderOffsetRad = absoluteEncoderOffset;
         this.absoluteEncoderReversed = absoluteEncoderReversed;
         absoluteEncoder = new CANCoder(absoluteEncoderId);
@@ -36,12 +40,67 @@ public class SwerveModule {
 
         driveEncoder = driveMotor.getEncoder();
         turningEncoder = turningMotor.getEncoder();
-        
+
         driveEncoder.setPositionConversionFactor(ModuleConstants.kDriveEncoderRot2Meter);
         driveEncoder.setVelocityConversionFactor(ModuleConstants.kDriveEncoderRPM2MeterPerSec);
         turningEncoder.setPositionConversionFactor(ModuleConstants.kTurningEncoderRot2Rad);
         turningEncoder.setVelocityConversionFactor(ModuleConstants.kTurningEncoderRPM2RadPerSec);
 
         turningPidController = new PIDController(ModuleConstants.kPTurning, 0, 0);
+
+        //Unchecked Code Below
+
+        turningPidController.enableContinuousInput(-Math.PI, Math.PI);
+
+        resetEncoders();
+    }
+
+    public double getDrivePosition() {
+        return driveEncoder.getPosition();
+    }
+
+    public double getTurningPosition() {
+        return turningEncoder.getPosition();
+    }
+
+    public double getDriveVelocity() {
+        return driveEncoder.getVelocity();
+    }
+
+    public double getTurningVelocity() {
+        return driveEncoder.getVelocity();
+    }
+
+    public double getAbsoluteEncoderRad() {
+        double angle = absoluteEncoder.getVoltage() / RobotController.getVoltage5V();
+        angle *= 2.0 * Math.PI;
+        angle -= absoluteEncoderOffsetRad;
+        return angle * (absoluteEncoderReversed ? -1.0 : 1.0);
+    }
+
+    public void resetEncoders() {
+        driveEncoder.setPosition(0);
+        turningEncoder.setPosition(getAbsoluteEncoderRad());
+    }
+
+    public SwerveModuleState getState() {
+        return new SwerveModuleState(getDriveVelocity(), new Rotation2d(getTurningPosition()));
+    }
+
+    public void setDesiredState(SwerveModuleState state) {
+        if (Math.abs(state.speedMetersPerSecond) < 0.001) {
+            stop();
+            return;
+        }
+        state = SwerveModuleState.optimize(state, getState().angle);
+        driveMotor.set(state.speedMetersPerSecond / DriveConstants.kPhysicalMaxSpeedMetersPerSecond);
+        turningMotor.set(turningPidController.calculate(getTurningPosition(), state.angle.getRadians()));
+        SmartDashboard.putString("Swerve[" + absoluteEncoder.getChannel() + "] state", state.toString());
+    }
+
+    public void stop() {
+        driveMotor.set(0);
+        turningMotor.set(0);
     }
 }
+
--- src/main/java/frc/robot/subsystems/swerve/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/swerve/SwerveSubsystem.java
@@ -3,35 +3,35 @@ package frc.robot.subsystems.swerve;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
 
-public class SwerveDriveSubsystem extends Subsystem {
+public class SwerveSubsystem extends Subsystem {
 
     // With eager singleton initialization, any static variables/fields used in the 
     // constructor must appear before the "INSTANCE" variable so that they are initialized 
     // before the constructor is called when the "INSTANCE" variable initializes.
 
     /**
-     * The Singleton instance of this SwerveDriveSubsystem. Code should use
+     * The Singleton instance of this SwerveSubsystem. Code should use
      * the {@link #getInstance()} method to get the single instance (rather
      * than trying to construct an instance of this class.)
      */
-    private final static SwerveDriveSubsystem INSTANCE = new SwerveDriveSubsystem();
+    private final static SwerveSubsystem INSTANCE = new SwerveSubsystem();
 
     /**
-     * Creates a new instance of this SwerveDriveSubsystem. This constructor
+     * Creates a new instance of this SwerveSubsystem. This constructor
      * is private since this class is a Singleton. Code should use
      * the {@link #getInstance()} method to get the singleton instance.
      */
-    private SwerveDriveSubsystem() {
+    private SwerveSubsystem() {
 
     }
 
     /**
-     * Returns the Singleton instance of this SwerveDriveSubsystem. This static method
+     * Returns the Singleton instance of this SwerveSubsystem. This static method
      * should be used, rather than the constructor, to get the single instance
-     * of this class. For example: {@code SwerveDriveSubsystem.getInstance();}
+     * of this class. For example: {@code SwerveSubsystem.getInstance();}
      */
     @SuppressWarnings("WeakerAccess")
-    public static SwerveDriveSubsystem getInstance() {
+    public static SwerveSubsystem getInstance() {
         return INSTANCE;
     }
 
