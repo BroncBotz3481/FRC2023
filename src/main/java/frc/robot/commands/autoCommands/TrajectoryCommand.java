06a259bfb080463015c3a4d516069fd395887905
svs2005 <56053155+svs2005@users.noreply.github.com>
2022-10-25 19:51:57 -0500

    DOES NOT WORK

---
+++ src/main/java/frc/robot/commands/autoCommands/TrajectoryCommand.java
@@ -0,0 +1,81 @@
+/****************************** Header ******************************\
+ Class Name: ExampleCommand extends CommandBase
+ File Name: ExampleCommand.java
+ Summary: An example command to use for learning and testing.
+ Project: BroncBotzFRC2023
+ Copyright (c) BroncBotz.
+ All rights reserved.
+
+ Author(s): Dylan Watson
+ Email: dylantrwatson@gmail.com
+ \********************************************************************/
+
+
+package frc.robot.commands.autoCommands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.index.IndexPolicy;
+import frc.robot.subsystems.index.IndexSubsystem;
+import frc.robot.subsystems.shooter.ShooterPolicy;
+import frc.robot.subsystems.shooter.ShooterSubsystem;
+import edu.wpi.first.wpilibj.Timer;
+
+/**
+ * An example command that uses an example subsystem.
+ */
+public class AutoPIDShot extends CommandBase {
+    @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+    private final ShooterSubsystem m_shooterSubsystem;
+    private Timer time;
+    private final IndexSubsystem m_indexSubsystem;
+    /**
+     * Creates a new ExampleCommand.
+     *
+     * @param subsystem The subsystem used by this command.
+     */
+    public AutoPIDShot(ShooterSubsystem subsystem, IndexSubsystem isubsystem) {
+        m_shooterSubsystem = subsystem;
+        m_indexSubsystem = isubsystem;
+        time = new Timer();
+        // Use addRequirements() here to declare subsystem dependencies.
+        addRequirements(subsystem);
+        addRequirements(isubsystem);
+    }
+
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {
+        time.start();
+        IndexPolicy.overridePressurePad = true;
+    }
+
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
+        m_shooterSubsystem.shootPID(12300);
+        if(ShooterPolicy.inBound(150)){
+            m_indexSubsystem.runIndex(-0.4);
+        }
+        
+    }
+
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {
+        time.stop();
+        time.reset();
+        IndexPolicy.overridePressurePad = false;
+        m_shooterSubsystem.stopShooter();
+
+    }
+
+
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        if(time.get()>=4){
+            return true;
+        }
+        return false;
+    }
+}
--- src/main/java/frc/robot/subsystems/drivetrain/DrivetrainPolicy.java
+++ src/main/java/frc/robot/subsystems/drivetrain/DrivetrainPolicy.java
@@ -11,19 +11,36 @@
  \********************************************************************/
 
 package frc.robot.subsystems.drivetrain;
-
+import frc.robot.Constants;
+import frc.robot.subsystems.drivetrain.DrivetrainSubsystem;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
 public final class DrivetrainPolicy {
     public static double powerLeft; // power for left motors
     public static double powerRight; // power for right motors
     public static double forwardPowerScale = 0.85;
     public static double backwardPowerScale = 0.7;
+    public static DifferentialDrive driveOdometry;
+    public static DifferentialDrive driveKinematics;
+    public static double rightSpeed;
+    public static double rightEncoderPosition, rightEncoderVelocity,
+    leftEncoderPosition, leftEncoderVelocity;
+    
+    rightEncoderVelocity = (rightEncoder.getVelocity() / 60) * Constants.wheelDiameter * Math.PI;
+    leftEncoderVelocity = (rightEncoder.getVelocity() / 60) * Constants.wheelDiameter * Math.PI;
+
+
+    public static double getRightVelocity()
+    {
+        return ((DrivetrainPolicy.rightSpeed * 60) / (Math.PI * Constants.wheelDiameter));
+    }
+
+
 
     
-    public static double rightEncoderPosition, rightEncoderVelocity,
-            leftEncoderPosition, leftEncoderVelocity;
+  
 
-
+  
     public static double setPowerScale()
     {
         
