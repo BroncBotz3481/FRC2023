495a9d1d0c483030ed574a420f8c334a71d39d8b
smartyvv <107887886+smartyvv@users.noreply.github.com>
2023-01-12 18:02:49 -0600

    Changed the navX initialization

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -18,6 +18,7 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.SPI.Port;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -53,6 +54,7 @@ public class Robot extends TimedRobot {
     private NetworkTableEntry area;
     private RobotContainer m_robotContainer;
     private UsbCamera m_usbCamera;
+    private AHRS navX;
 
     /**
      * This function is run when the robot is first started up and should be used for any
@@ -73,9 +75,10 @@ public class Robot extends TimedRobot {
         skew = table.getEntry("skew");
         area = table.getEntry("area");
 
+        navX = new AHRS(Port.kMXP);
+
         m_robotContainer = new RobotContainer();
-        
-        AHRS navX = new AHRS();
+
         while(navX == null)
         {
             System.out.println("NavX not connected.");
