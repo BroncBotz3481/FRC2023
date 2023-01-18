1ae9c7b73e06f317805e5509a5ce55ada94a3912
Zamoa0714 <samuelzhao0714@gmail.com>
2023-01-17 19:14:31 -0600

    Baseline for commandbased robot for next year

---
+++ src/main/java/frc/robot/subsystems/wrist/WristPolicy.java
@@ -0,0 +1,18 @@
+/****************************** Header ******************************\
+ Class Name: IntakePolicyClass
+ File Name: IntakePolicy.java
+ Summary: Contains constant subclasses and variables for commands, subsystems, and utility methods
+ Project: BroncBotzFRC2023
+ Copyright (c) BroncBotz.
+ All rights reserved.
+
+ Author(s): Shruti Venkat and Samuel Zhao
+ Email: Shruti.venkat05@gmail.com
+ \********************************************************************/
+
+package frc.robot.subsystems.wrist;
+
+
+public final class WristPolicy {
+
+}
